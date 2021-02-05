509bd780860d8d228de19004a91ca3c0cab78613
twozeronine <amdusias209@gmail.com>
2021-02-05 15:28:34 +0900

    Add Component-event

--- src/App.js
+++ src/App.js
@@ -5,15 +5,51 @@ import Content from "./components/Content";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: "read",
+      selected_content_id: 2,
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      welcome: { title: "Welcome", desc: "Hello, React!!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" },
+      ],
+    };
+  }
+
   render() {
+    let _title = null;
+    let _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      let data = this.state.contents.find(
+        (element) => element.id === this.state.selected_content_id
+      );
+      _title = data.title;
+      _desc = data.desc;
+    }
+
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language."
-        ></Content>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: "welcome" });
+          }.bind(this)}
+        ></Subject>
+        <TOC
+          onChangePage={function (id) {
+            this.setState({ mode: "read", selected_content_id: Number(id) });
+          }.bind(this)}
+          data={this.state.contents}
+        ></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,19 +2,29 @@ import React, { Component } from "react";
 
 class TOC extends Component {
   render() {
+    const lists = [];
+    let data = this.props.data;
+
+    for (let i = 0; i < data.length; i++) {
+      lists.push(
+        <li key={data[i].id}>
+          <a
+            href={"/content/" + data[i].id}
+            data-id={data[i].id}
+            onClick={function (e) {
+              e.preventDefault();
+              this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}
+          >
+            {data[i].title}
+          </a>
+        </li>
+      );
+    }
+
     return (
       <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
+        <ul>{lists}</ul>
       </nav>
     );
   }
